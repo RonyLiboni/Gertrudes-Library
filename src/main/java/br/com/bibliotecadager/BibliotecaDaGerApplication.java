c289db4eadc9ec4b38a2e15322edffccc09f542c
RonaldLiboni <ronald.liboni@acad.pucrs.br>
2022-08-10 09:45:23 -0300

    Primeiro commit com dependencias iniciais do projeto

---
+++ src/main/java/br/com/bibliotecadager/BibliotecaDaGerApplication.java
@@ -0,0 +1,13 @@
+package br.com.bibliotecadager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaDaGerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaDaGerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bibliotecadager/BibliotecaDaGerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bibliotecadager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaDaGerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
