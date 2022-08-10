004639b644e623f3cb95ed04d683fbfc84a2b75a
RonaldLiboni <ronald.liboni@acad.pucrs.br>
2022-08-10 16:23:21 -0300

    FEAT (EmployeeService) saves employee in database

---
+++ src/main/java/br/com/gers_library/service/employee/EmployeeService.java
@@ -0,0 +1,29 @@
+package br.com.gers_library.service.employee;
+
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+import br.com.gers_library.entities.employee.Employee;
+import br.com.gers_library.entities.employee.dto.EmployeeDto;
+import br.com.gers_library.entities.employee.dto.EmployeeFormDto;
+import br.com.gers_library.repositories.EmployeeRepository;
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class EmployeeService {
+	
+	private final EmployeeRepository employeeRepository;
+	private final ModelMapper modelMapper;
+	
+	public EmployeeDto registerEmployee(EmployeeFormDto form) {
+		return saveInDataBase(modelMapper.map(form, Employee.class));
+	}
+	
+	@Transactional
+	public Employee saveInDataBase(Employee employee) {
+		return employeeRepository.save(employee);
+	}
+	
+}
