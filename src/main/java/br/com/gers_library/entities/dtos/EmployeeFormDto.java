df2090a45f0e76fbd0f386e22d9bec47f59138b4
RonaldLiboni <ronald.liboni@acad.pucrs.br>
2022-08-10 15:15:59 -0300

    UPDATE (EmployeeFormDto) creates employeeFormDto

---
+++ src/main/java/br/com/gers_library/entities/dtos/EmployeeFormDto.java
@@ -0,0 +1,41 @@
+package br.com.gers_library.entities.dtos;
+
+import java.time.LocalDate;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import br.com.gers_library.entities.Employee;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmployeeFormDto {
+	
+	@NotBlank
+	private String fullName;
+	@NotBlank
+	private String documentCpf;
+	@NotBlank 
+	private String cep;
+	@NotBlank
+	private String jobTitle;
+	@NotNull
+	private LocalDate hireDate;
+	
+	public EmployeeFormDto(Employee employee) {
+		this.fullName = employee.getFullName();
+		this.documentCpf = employee.getDocumentCpf();
+		this.cep = employee.getAddress().getCep();
+		this.jobTitle = employee.getJobTitle();
+		this.hireDate = employee.getHireDate();
+	}
+
+}
