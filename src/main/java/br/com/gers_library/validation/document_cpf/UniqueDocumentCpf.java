8b27eb82973c5ed718edfbc5281e502a8869a7e0
RonaldLiboni <ronald.liboni@acad.pucrs.br>
2022-08-12 09:16:25 -0300

    FEAT (@UniqueDocumentCpf) validates the field documentCpf

---
+++ src/main/java/br/com/gers_library/validation/document_cpf/UniqueDocumentCpf.java
@@ -0,0 +1,24 @@
+package br.com.gers_library.validation.document_cpf;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = DocumentCpfValidator.class)
+public @interface UniqueDocumentCpf {
+	
+	String message() default "You must write a valid CPF!";
+
+	boolean required() default true;
+
+	Class<?>[] groups() default {};
+
+	Class<? extends Payload>[] payload() default {};
+
+}
