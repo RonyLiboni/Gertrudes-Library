7c17c17da6105ee2058d17582eef17103c4eb98e
RonaldLiboni <ronald.liboni@acad.pucrs.br>
2022-08-12 10:46:26 -0300

    FIX (ViaCepAddress) adds @NoArgs and @AllArgs Constructor

--- src/main/java/br/com/gers_library/http/ViaCepAddress.java
+++ src/main/java/br/com/gers_library/http/ViaCepAddress.java
@@ -1,12 +1,16 @@
 package br.com.gers_library.http;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ViaCepAddress {
 	
 	private String cep;
