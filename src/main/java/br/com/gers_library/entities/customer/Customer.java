a64766f996245fa660cbb213365278044ecd11b7
RonaldLiboni <ronald.liboni@acad.pucrs.br>
2022-08-12 21:00:57 -0300

    FEAT (Customer) add AllArgsConstructor

--- src/main/java/br/com/gers_library/entities/customer/Customer.java
+++ src/main/java/br/com/gers_library/entities/customer/Customer.java
@@ -3,16 +3,23 @@ package br.com.gers_library.entities.customer;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+import br.com.gers_library.entities.library_user.Address;
 import br.com.gers_library.entities.library_user.LibraryUser;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-@Builder
 @Table(name = "customers")
+@NoArgsConstructor
 public class Customer extends LibraryUser {
+	
+	@Builder
+	public Customer(Long id, String fullName, String documentCpf, Address address) {
+		super(id, fullName, documentCpf, address);
+	}
 	
 }
