a9ef49bb6000ff96f9caabe5c4399a6b52cf8cd7
RonaldLiboni <ronald.liboni@acad.pucrs.br>
2022-08-12 22:19:12 -0300

    FIX (CepValidator) if cpf size is different than 8 and RestTemplate doesn't receive ViaCepAddress.class return false

--- src/main/java/br/com/gers_library/validation/cep/CepValidator.java
+++ src/main/java/br/com/gers_library/validation/cep/CepValidator.java
@@ -2,33 +2,38 @@ package br.com.gers_library.validation.cep;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
-import org.springframework.http.HttpStatus;
+
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
-import br.com.gers_library.http.ViaCepAddress;
 
-public class CepValidator implements ConstraintValidator<CepExists, String>{
+import br.com.gers_library.http.ViaCepAddress;
+import br.com.gers_library.service.template.ServiceTemplate;
+
+public class CepValidator extends ServiceTemplate implements ConstraintValidator<CepExists, String>{
 	
 	private final String url = "https://viacep.com.br/ws/";
 	
 	@Override
 	public boolean isValid(String cep, ConstraintValidatorContext context) {
-		if(cep == null)
-			return false;
-		
-		cep = cep.replaceAll("[^0-9]", "");
-		
-		if (getStatusCodeFromViaCep(cep) == HttpStatus.OK)
+		if (doesCepExist(cep) == true)
 			return true;
 				
 		return false;
 	}
 	
-	private HttpStatus getStatusCodeFromViaCep(String cep) {
+	private boolean doesCepExist(String cep) {		
+		if(cep == null)
+			return false;
+		
+		cep = letStringOnlyWithNumbers(cep);
+	
+		if (cep.length() != 8)
+			return false;
+		
 		try {
-			return new RestTemplate().getForEntity(url+ cep + "/json/", ViaCepAddress.class).getStatusCode();
+			return new RestTemplate().getForEntity(url+ cep + "/json/", ViaCepAddress.class).getBody().getLocalidade() != null;
 		} catch (RestClientException e) {
-			return HttpStatus.BAD_REQUEST;
+			return false;
 		}
 	}
 
