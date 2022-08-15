2940de0611fe8d6d0c6b78430d1e6036868011a9
RonaldLiboni <ronald.liboni@acad.pucrs.br>
2022-08-15 08:13:39 -0300

    FEAT (CustomerRepository) adds query to get ordered cep count list

--- src/main/java/br/com/gers_library/repositories/customer/CustomerRepository.java
+++ src/main/java/br/com/gers_library/repositories/customer/CustomerRepository.java
@@ -1,9 +1,18 @@
 package br.com.gers_library.repositories.customer;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.com.gers_library.entities.customer.Customer;
+import br.com.gers_library.repositories.projections.HighestIncidenceCepProjection;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-
+	
+	@Query(value = "SELECT cep, street, district, COUNT(cep) as cepCount "
+			+ "FROM customers "
+			+ "GROUP BY cep "
+			+ "ORDER BY cepCount DESC ", nativeQuery = true)
+	 List<HighestIncidenceCepProjection> getOrderedCepCount();
 }
