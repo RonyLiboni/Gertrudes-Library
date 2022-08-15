5f3ede4bf27856f7b13e984dacaaec4a39522716
RonaldLiboni <ronald.liboni@acad.pucrs.br>
2022-08-15 17:25:38 -0300

    DOC CustomerController and EmployeeController fixes the mistype in status code and pageableDefault

--- src/main/java/br/com/gers_library/controllers/customer/CustomerController.java
+++ src/main/java/br/com/gers_library/controllers/customer/CustomerController.java
@@ -23,6 +23,7 @@ import br.com.gers_library.entities.customer.dto.CustomerFormDto;
 import br.com.gers_library.repositories.projections.HighestIncidenceCepProjection;
 import br.com.gers_library.service.customer.CustomerService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -30,7 +31,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/v1/admin/customer")
 @RequiredArgsConstructor
-@Tag(name = "Customer Endpoints", description = "Here you will be able to do all CRUD operations and get the cep with highest incidence from customers.")
+@Tag(name = "Customer Endpoints", description = "Here, you will be able to do all CRUD operations and get the cep with highest incidence from customers.")
 public class CustomerController {
 	
 	private final CustomerService customerService;
@@ -45,7 +46,7 @@ public class CustomerController {
 	@GetMapping
 	@Operation(summary = "Gets all Customers.")
 	@ApiResponse(responseCode= "200", description = "The resources was got with success!")
-	public ResponseEntity<Page<CustomerDto>> getAllDto(@PageableDefault() Pageable page){
+	public ResponseEntity<Page<CustomerDto>> getAllDto(@PageableDefault()@Parameter(hidden=true) Pageable page){
 		return ResponseEntity.status(HttpStatus.OK).body(customerService.getAllDto(page));
 	}
 	
@@ -58,7 +59,7 @@ public class CustomerController {
 	
 	@DeleteMapping("/{id}")	
 	@Operation(summary = "Deletes an Customers by id.")
-	@ApiResponse(responseCode= "202", description = "The resource was deleted with success!")
+	@ApiResponse(responseCode= "204", description = "The resource was deleted with success!")
 	public ResponseEntity<?> deleteById(Long id){
 		customerService.deleteById(id);
 		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
--- src/main/java/br/com/gers_library/controllers/employee/EmployeeController.java
+++ src/main/java/br/com/gers_library/controllers/employee/EmployeeController.java
@@ -23,6 +23,7 @@ import br.com.gers_library.entities.employee.dto.EmployeeFormDto;
 import br.com.gers_library.repositories.projections.HighestIncidenceCepProjection;
 import br.com.gers_library.service.employee.EmployeeService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -45,7 +46,7 @@ public class EmployeeController {
 	@GetMapping
 	@Operation(summary = "Gets all Employees.")
 	@ApiResponse(responseCode= "200", description = "The resources was got with success!")
-	public ResponseEntity<Page<EmployeeDto>> getAllDto(@PageableDefault() Pageable page){
+	public ResponseEntity<Page<EmployeeDto>> getAllDto(@PageableDefault() @Parameter(hidden=true) Pageable page){
 		return ResponseEntity.status(HttpStatus.OK).body(employeeService.getAllDto(page));
 	}
 	
@@ -58,7 +59,7 @@ public class EmployeeController {
 	
 	@DeleteMapping("/{id}")	
 	@Operation(summary = "Deletes an Employees by id.")
-	@ApiResponse(responseCode= "202", description = "The resource was deleted with success!")
+	@ApiResponse(responseCode= "204", description = "The resource was deleted with success!")
 	public ResponseEntity<?> deleteById(Long id){
 		employeeService.deleteById(id);
 		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
