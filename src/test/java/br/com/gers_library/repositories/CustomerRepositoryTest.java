b2dba4a755cc41b43cfe523e005dd1f477972442
RonaldLiboni <ronald.liboni@acad.pucrs.br>
2022-08-16 17:52:18 -0300

    TEST (CustomerRepositoryTest) adds test for the method getOrderedCepCount

--- src/test/java/br/com/gers_library/repositories/CustomerRepositoryTest.java
+++ src/test/java/br/com/gers_library/repositories/CustomerRepositoryTest.java
@@ -2,30 +2,33 @@ package br.com.gers_library.repositories;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import br.com.gers_library.configurations.RepositoryTestTemplate;
-import br.com.gers_library.entities.customer.Customer;
+import br.com.gers_library.entities.employee.Employee;
 import br.com.gers_library.entities.library_user.Address;
-import br.com.gers_library.repositories.customer.CustomerRepository;
+import br.com.gers_library.repositories.employee.EmployeeRepository;
 import br.com.gers_library.repositories.projections.HighestIncidenceCepProjection;
-import br.com.gers_library.util.CustomerUtil;
+import br.com.gers_library.util.EmployeeUtil;
 
-class EmployeeRepositoryTest extends RepositoryTestTemplate  {
+class CustomerRepositoryTest extends RepositoryTestTemplate  {
 	
 	@Autowired
-	private CustomerRepository customerRepository;
+	private EmployeeRepository employeeRepository;
 	
 	@Test
 	void getOrderedCepCount_ShouldReturnAListWithTheHighestIncidenceCepInTheIndex0_WhenSuccessful() {	
-		customerRepository.save(CustomerUtil.buildValidatedEntity());
-		customerRepository.save(CustomerUtil.buildValidatedEntity());
-		customerRepository.save(Customer.builder()
+		employeeRepository.save(EmployeeUtil.buildValidatedEntity());
+		employeeRepository.save(EmployeeUtil.buildValidatedEntity());
+		employeeRepository.save(Employee.builder()
 				.fullName("XXXX test 2")
 				.documentCpf("XXXX")
+				.hireDate(LocalDate.now())
+				.jobTitle("XXXX")
 				.address(Address.builder()
 							.cep("XXXX")
 							.city("São Paulo")
@@ -35,8 +38,7 @@ class EmployeeRepositoryTest extends RepositoryTestTemplate  {
 							.complement("lado ímpar")
 							.build())
 				.build());
-		
-		List<HighestIncidenceCepProjection> orderedCepCount = customerRepository.getOrderedCepCount();
+		List<HighestIncidenceCepProjection> orderedCepCount = employeeRepository.getOrderedCepCount();
 		assertThat(orderedCepCount.get(0).getCepCount()).isEqualTo(2);
 		assertThat(orderedCepCount.get(1).getCepCount()).isEqualTo(1);
 		assertThat(orderedCepCount.size()).isEqualTo(2);
