315afb18363302c893c4ed9e55523337ea7482b8
RonyLiboni <ronald.liboni@acad.pucrs.br>
2022-09-07 09:19:24 -0300

    FIX adds fetch type as eager in Role

--- src/main/java/br/com/gers_library/entities/user/Role.java
+++ src/main/java/br/com/gers_library/entities/user/Role.java
@@ -1,15 +1,13 @@
 package br.com.gers_library.entities.user;
 
-import java.util.UUID;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
 import org.springframework.security.core.GrantedAuthority;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -25,10 +23,11 @@ public class Role implements GrantedAuthority{
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private UUID roleId;
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long roleId;
 	
 	@Column(nullable = false, unique = true)
+	@Enumerated(EnumType.STRING)
 	private RoleName roleName;
 
 	@Override
