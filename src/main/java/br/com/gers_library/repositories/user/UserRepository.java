f48bffc569f8de5b89b704482708efcf6b6c65d3
RonyLiboni <ronald.liboni@acad.pucrs.br>
2022-09-07 09:22:43 -0300

    FIX (User) changes the name of the class because of the conflict with spring classes

--- src/main/java/br/com/gers_library/repositories/user/UserRepository.java
+++ src/main/java/br/com/gers_library/repositories/user/UserRepository.java
@@ -6,11 +6,11 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import br.com.gers_library.entities.user.User;
+import br.com.gers_library.entities.user.UserEntity;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, UUID> {
-	Optional<User> findByUsername(String username);
+public interface UserRepository extends JpaRepository<UserEntity, UUID> {
+	Optional<UserEntity> findByUsername(String username);
 	
 	boolean existsByUsername(String username);
 }
