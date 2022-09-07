7ced5bf573741f788650b74c1b16bca290bb12c7
RonyLiboni <ronald.liboni@acad.pucrs.br>
2022-09-07 09:39:29 -0300

    FEAT implements security to endpoints

---
+++ src/main/java/br/com/gers_library/configurations/spring_doc/SpringDocConfig.java
@@ -0,0 +1,21 @@
+package br.com.gers_library.configurations.spring_doc;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class SpringDocConfig {
+	@Bean
+	 public OpenAPI customOpenAPI() {
+	   return new OpenAPI()
+	          .components(new Components()
+	          .addSecuritySchemes("bearer-key",
+	          new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("Bearer").bearerFormat("JWT")));
+	}
+	
+	
+}
