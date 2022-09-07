a06c175058b3f64d19652bf6c1df04eba77378a9
RonyLiboni <ronald.liboni@acad.pucrs.br>
2022-09-07 09:26:24 -0300

    FEAT adds Login, tokenDto, TokenService and adds properties to use with spring security

---
+++ src/main/java/br/com/gers_library/entities/token/TokenDto.java
@@ -0,0 +1,17 @@
+package br.com.gers_library.entities.token;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TokenDto {
+
+	private String token;
+	private String type;
+
+}
