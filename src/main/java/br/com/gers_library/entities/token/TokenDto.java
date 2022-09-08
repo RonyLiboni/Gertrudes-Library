d2f3e165f9770fe51297b0c79a6417dfeed0bf5a
RonyLiboni <ronald.liboni@acad.pucrs.br>
2022-09-08 15:39:52 -0300

    DOC (TokenDto) adds schema to tokenDto

--- src/main/java/br/com/gers_library/entities/token/TokenDto.java
+++ src/main/java/br/com/gers_library/entities/token/TokenDto.java
@@ -1,5 +1,6 @@
 package br.com.gers_library.entities.token;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,8 +11,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class TokenDto {
-
+	
+	@Schema(example = "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiO...")
 	private String token;
+	@Schema(example = "Bearer")
 	private String type;
 
 }
